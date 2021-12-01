195defe8f4ac2c3b565baeee6257c9604e567289
ruslan <rus.magamedov.99@bk.ru>
2021-12-01 15:02:09 +0300

    go

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -19,8 +19,8 @@ const Navbar = () => {
               <button onClick={showMenu} className="xl:hidden">
                 <img src={iconMenu} alt="bar" />
               </button>
-              <img src={logo} alt="sneakers" className="xl:w-44" />
-              <ul className="hidden xl:flex gap-x-10 text-xl text-neutral-darkGrayishBlue">
+              <img src={logo} alt="sneakers" className="xl:w-36" />
+              <ul className="hidden xl:flex gap-x-10 text-base text-neutral-darkGrayishBlue">
                 {Links.map((link) => {
                   const { name, to, id } = link;
                   return (
@@ -31,19 +31,19 @@ const Navbar = () => {
                 })}
               </ul>
             </div>
-            <div className="flex items-center gap-x-5 xl:gap-x-16">
+            <div className="flex items-center gap-x-5 xl:gap-x-12">
               <button className="relative" onClick={toggleCart}>
                 {cartItems.length !== 0 ? (
                   <span className="absolute -top-1 bg-primary-orange text-neutral-white rounded-full px-2 text-xs font-bold">
                     {totalAmount}
                   </span>
                 ) : null}
-                <img src={cartIcon} alt="cart" className="xl:w-8" />
+                <img src={cartIcon} alt="cart" className="xl:w-6" />
               </button>
               <img
                 src={avatar}
                 alt="avatar"
-                className="w-6 hover:border-primary-orange cursor-pointer hover:border-solid xl:border-4 rounded-full xl:w-16 transition-colors"
+                className="w-6 hover:border-primary-orange cursor-pointer hover:border-solid xl:border-2 rounded-full xl:w-14 transition-colors"
               />
             </div>
           </div>
